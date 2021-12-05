32bbdc05c17bdf4ac4562f94579a039afc777279
Philip Ivanov <philip.i@outlook.com>
2021-12-05 23:59:17 +0300

    Added new UI view

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Philip Ivanov on 5.12.21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
